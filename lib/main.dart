6671779e14d38a33aed274fc0b3e0d3f197a981f
xbrymo06 <xbrymo06@gmail.com>
2024-12-23 05:08:19 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+library hafezcodex;
+export './widgets/album_art.dart';
+export './widgets/audio_appbar.dart';
+export './widgets/song_info.dart';
+export './widgets/volume.dart';
+export './extension/format_extension.dart';
